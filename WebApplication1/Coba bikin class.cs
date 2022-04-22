61f7221c9f70d63ac970c005b39bfe4b53ed7aeb
ElfSinaga <elfridals12@gmail.com>
2022-04-22 17:47:34 +0800

    testing

--- WebApplication1/Coba bikin class.cs
+++ WebApplication1/Coba bikin class.cs
@@ -2,5 +2,12 @@
 {
     public class Class
     {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 30 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
     }
 }
